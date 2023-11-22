4d59ee3d1a5ad6664eac9e675bc388f1db374744
Adenn Tumba <adinn.tumba@hotmail.com>
2023-11-22 17:59:07 -0300

    change name of variables

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@
  * }
  * ```
  */
-
+#tfsec:ignore:aws-s3-enable-bucket-encryption tfsec:ignore:aws-s3-enable-bucket-logging tfsec:ignore:aws-s3-block-public-acls tfsec:ignore:aws-s3-block-public-policy tfsec:ignore:aws-s3-ignore-public-acls tfsec:ignore:aws-s3-no-public-buckets tfsec:ignore:aws-s3-encryption-customer-key tfsec:ignore:aws-s3-enable-versioning tfsec:ignore:aws-iam-no-user-attached-policies tfsec:ignore:aws-s3-specify-public-access-block
 resource "aws_s3_bucket" "bucket" {
   count  = var.enable ? 1 : 0
   bucket = join("-", ["weni", var.environment, var.bucketname])
