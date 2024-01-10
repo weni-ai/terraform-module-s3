b4d2ad86732341f1755f006a63b66a67282871bf
Icaro Rodrigues <icaro.rodrigues@protonmail.com>
2024-01-10 18:14:09 -0300

    format output to better visibility

--- _outputs.tf
+++ _outputs.tf
@@ -1,9 +1,10 @@
 output "s3_access_info" {
   description = "IAM role name, S3 bucket name, and user access keys with permissions"
   value = {
-    access_key = try(aws_iam_access_key.bucket_access_key[0].id, null),
-    secret_key = try(nonsensitive(aws_iam_access_key.bucket_access_key[0].secret), null),
-    eks_roles  = try(module.iam_eks_role[*].iam_role_arn, null),
-    name       = try(var.bucket_name, null)
+    (var.bucket_name) = {
+      aws_access_key = try(aws_iam_access_key.bucket_access_key[0].id, null),
+      aws_secret_key = try(nonsensitive(aws_iam_access_key.bucket_access_key[0].secret), null),
+      eks_role       = try(module.iam_eks_role[0].iam_role_arn, null),
+    }
   }
 }
