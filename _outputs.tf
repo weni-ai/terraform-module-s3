2ab5f5ebffe2f73d4d6fe8b70c3356d87c40440d
Icaro Rodrigues <icaro.rodrigues@protonmail.com>
2024-01-11 16:18:54 -0300

    fix output to be more easily used

--- _outputs.tf
+++ _outputs.tf
@@ -1,10 +1,14 @@
-output "s3_access_info" {
-  description = "IAM role name, S3 bucket name, and user access keys with permissions"
-  value = {
-    (var.bucket_name) = {
-      aws_access_key = try(aws_iam_access_key.bucket_access_key[0].id, null),
-      aws_secret_key = try(nonsensitive(aws_iam_access_key.bucket_access_key[0].secret), null),
-      eks_role       = try(module.iam_eks_role[0].iam_role_arn, null),
-    }
-  }
+output "aws_access_key" {
+  description = "IAM access key for S3 bucket"
+  value       = try(aws_iam_access_key.bucket_access_key[0].id, null)
+}
+
+output "aws_secret_key" {
+  description = "IAM secret key for S3 bucket"
+  value       = try(nonsensitive(aws_iam_access_key.bucket_access_key[0].secret), null)
+}
+
+output "eks_role" {
+  description = "IAM role ARN for EKS cluster"
+  value       = try(module.iam_eks_role[0].iam_role_arn, null)
 }
