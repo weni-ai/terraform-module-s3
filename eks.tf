d307f8a72d3be5438054700ac2a307c5326fabea
Psycho Mantys <psycho.mantys@gmail.com>
2024-10-22 19:50:09 -0300

    fix

--- eks.tf
+++ eks.tf
@@ -5,9 +5,11 @@ module "iam_eks_role" {
 
   role_name                = var.bucket_name
   cluster_service_accounts = var.create_iam_eks_role
-  role_policy_arns = merge({
-    for idx, policy in aws_iam_policy.bucket_rw : aws_iam_policy.bucket_rw[idx].name => aws_iam_policy.bucket_rw[idx].arn
-  },{
-    for idx, policy in aws_iam_policy.extra_custom_policy : aws_iam_policy.extra_custom_policy[idx].name => aws_iam_policy.extra_custom_policy[idx].arn
-  })
+  role_policy_arns = merge(
+    {
+      for idx, policy in aws_iam_policy.bucket_rw : aws_iam_policy.bucket_rw[idx].name => aws_iam_policy.bucket_rw[idx].arn
+    }, {
+      for idx, policy in aws_iam_policy.extra_custom_policy : aws_iam_policy.extra_custom_policy[idx].name => aws_iam_policy.extra_custom_policy[idx].arn
+    }
+  )
 }
